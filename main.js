342e813e2949972ad1a980d823afd57f18e549af
dra-0428 <ryuki06030428@gmail.com>
2025-04-22 14:40:58 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('ペアコード起動');
+const hint = "南国 + 赤い果実";
+const answer = "パイナップル";
+
+function showHint() {
+  document.getElementById("hintArea").innerText = "ヒント: " + hint;
+}
+
+function checkAnswer() {
+  const guess = document.getElementById("guess").value.trim();
+  if (guess === answer) {
+    document.getElementById("result").innerText = "正解！シンクロ率100%！";
+  } else {
+    document.getElementById("result").innerText = "不正解...もう一度試してね。";
+  }
+}
